cf4742a81978476b31d9f4a8f8db12d696b036a8
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-17 16:57:47 +0800

    clear code

--- frontend/src/pages/My/index.tsx
+++
@@ -1,6 +0,0 @@
-import styled from "@emotion/styled"
-
-
-export default function MyPage() {
-    return <div></div>
-}
--- server/src/api/event-guest/routes/event-guest.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * event-guest router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::event-guest.event-guest');
