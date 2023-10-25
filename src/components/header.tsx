ae1e871d90b7c50a96c88a617bdb04d2e2ecb1b2
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-25 20:20:12 +0800

    add package and update header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,19 +1,53 @@
 import styled from "@emotion/styled";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import Logo from "../assets/images/logo.jpg";
+
 export default function Header() {
   return (
     <HeaderStyle>
-      <a href="/">Home</a>
-      <a href="/create_house">Create House</a>
+      <nav>
+        <div className="left">
+          <Link to="/">
+            <img src={Logo} alt="" />
+          </Link>
+          <Link to="/explore/hacker-houses">Houses</Link>
+          <Link to="/explore/projects">Projects</Link>
+          <Link to="/explore/hackers">Hackers</Link>
+          <Link to="/how-to-docs">Guidance</Link>
+        </div>
+        <div>
+          <Link to="/create_house">
+            <Button>Create</Button>
+          </Link>
+        </div>
+      </nav>
     </HeaderStyle>
   );
 }
 
 const HeaderStyle = styled.header`
+  nav {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    max-width: 1280px;
+    margin: 0 auto;
+    a {
+      height: 60px;
+    }
+    .left {
+      display: flex;
+      gap: 40px;
+    }
+    img {
+      height: 50px;
+      position: relative;
+      top: 5px;
+    }
+  }
   width: 100%;
   height: 60px;
   border-bottom: 1px solid #ddd;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  gap: 20px;
+  line-height: 60px;
 `;
