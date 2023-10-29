a6c09e3b39fd2c1f8a5c825a8cd04af6dec0bb45
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-10-29 21:55:38 +0800

    update album-item

--- src/components/albumItem.tsx
+++ src/components/albumItem.tsx
@@ -3,17 +3,21 @@ import Card from "@mui/material/Card";
 
 export default function AlbumItem() {
   return (
-    <AlbumItemStyled raised>
+    <AlbumItemStyled>
       <img
         src="https://www.stylearena.net/wp-content/uploads/2015/04/cute-cat-in-jeans-1024x640.jpg"
         alt=""
       />
+      <div className="name">Album name</div>
     </AlbumItemStyled>
   );
 }
 
-const AlbumItemStyled = styled(Card)`
+const AlbumItemStyled = styled.div`
   img {
     width: 100%;
+  }
+  .name {
+    text-align: center;
   }
 `;
