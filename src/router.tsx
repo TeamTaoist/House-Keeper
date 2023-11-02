595b1840399d5ed21f60502f17d44512db870652
CatFlow <22841916+Meow711@users.noreply.github.com>
2023-11-02 21:53:00 +0800

    add toast package

--- src/router.tsx
+++ src/router.tsx
@@ -2,16 +2,18 @@ import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import HomePage from "./pages/home";
 import CreateHousePage from "./pages/create-hacker-house";
-import ApplyPage from "./pages/apply";
 import ExploreHouses from "./pages/explore/houses";
 import ExploreHackers from "./pages/explore/hackers";
 import ExploreProjects from "./pages/explore/projects";
 import HackerHouseInfoPage from "./pages/hacker-house/info";
+import ApplyPage from "./pages/hacker-house/apply";
 import ProjectInfoPage from "./pages/project/info";
 import Profile from "./pages/profile";
 import ManageHackerHouseIndex from "./pages/manage/index";
 import ManageHackerHouse from "./pages/manage/hackerHouse";
 import GuidancePage from "./pages/guidancePage";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
 
 export default function RouterLink() {
   return (
@@ -25,9 +27,10 @@ export default function RouterLink() {
         <Route path="/explore/projects" element={<ExploreProjects />} />
         <Route path="/create-hacker-house" element={<CreateHousePage />} />
         <Route path="/hacker-house/:id" element={<HackerHouseInfoPage />} />
+        <Route path="/hacker-house/:id/apply" element={<ApplyPage />} />
+
         <Route path="/project/:id" element={<ProjectInfoPage />} />
 
-        <Route path="/apply" element={<ApplyPage />} />
         {/* user */}
         <Route path="/profile" element={<Profile />} />
         <Route
@@ -39,6 +42,18 @@ export default function RouterLink() {
           element={<ManageHackerHouse />}
         />
       </Routes>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </Router>
   );
 }
